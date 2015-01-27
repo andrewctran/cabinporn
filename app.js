3285a84c692b3bab29dc907f5b50ed8b5433a93a
Andrew Tran <atran@princeton.edu>
2015-01-27 02:15:29 -0500

    making calls to Tumblr API

---
+++ app.js
@@ -0,0 +1,14 @@
+$.ajax({
+    url: "http://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50",
+    dataType: 'jsonp',
+    success: function(results){
+        console.log(results); 
+        var rand = Math.floor(Math.random() * 50);
+        while (results.response.posts[rand].photos[0].original_size.width < 1280) {
+        	rand = Math.floor(Math.random() * 50);
+        }
+        var url = results.response.posts[rand].photos[0].original_size.url;
+        $('body').css('background-image', 'url('+url+')');
+        console.log(url);
+    }
+});
