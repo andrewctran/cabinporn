b364e0bde745896072783faefccab8c21b87b240
Andrew Tran <atran@princeton.edu>
2015-01-27 02:22:27 -0500

    added retrieval offset

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var offset = Math.floor(Math.random() * 1200);
+
 $.ajax({
-    url: "http://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50",
+    url: "http://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50&offset="+offset,
     dataType: 'jsonp',
     success: function(results){
+    	console.log(url)
         console.log(results); 
         var rand = Math.floor(Math.random() * 50);
         while (results.response.posts[rand].photos[0].original_size.width < 1280) {
