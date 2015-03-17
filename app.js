a0a572b982378ffe770511aac53c49e186d4e9b7
Andrew Tran <atran@princeton.edu>
2015-03-17 00:34:24 -0400

    add grunt task to tidy css; clean up captions; working shuffle

--- app.js
+++ app.js
@@ -11,7 +11,29 @@ $.ajax({
         	rand = Math.floor(Math.random() * 50);
         }
         var url = results.response.posts[rand].photos[0].original_size.url;
-        $('body').css('background', 'linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url('+url+')');
+        var post_url = results.response.posts[rand].post_url;
+        var caption = results.response.posts[rand].caption;
+        document.getElementById("cover-image").style.backgroundImage = 'url(' +url+ ')';
+        document.getElementById("link-cover").href= post_url;
+        document.getElementById("title").href= post_url;
         console.log(url);
+
+        var temp = document.createElement("div");
+        temp.innerHTML = caption;
+
+        // TODO: check if caption is still too long
+        document.getElementById("title").innerHTML = stripHTML(caption.substring(0, caption.indexOf('.')));
+
+        // var link = document.createElement('a');
+        // link.href = post_url;
+        // link.innerHTML = caption;
+        // document.querySelectorAll('span.name')[0].appendChild(link).appendChild(link.previousSibling);
+
     }
 });
+
+function stripHTML(html) {
+    var temp = document.createElement("div");
+    temp.innerHTML = html;
+    return temp.innerText;
+}
