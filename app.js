bcb28d152d056aa572b2a961a24c764e7d1c78ea
Andrew Tran <atran@princeton.edu>
2015-03-17 23:07:54 -0400

    implements regex matching for contributor info; clean up directory

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $.ajax({
     url: "http://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50&offset="+offset,
     dataType: 'jsonp',
     success: function(results){
-    	console.log(url)
-        console.log(results); 
+    	// console.log(url)
+     //    console.log(results); 
         var rand = Math.floor(Math.random() * 50);
         while (results.response.posts[rand].photos[0].original_size.width < 1280) {
         	rand = Math.floor(Math.random() * 50);
@@ -16,20 +16,15 @@ $.ajax({
         document.getElementById("cover-image").style.backgroundImage = 'url(' +url+ ')';
         document.getElementById("link-cover").href= post_url;
         document.getElementById("title").href= post_url;
-        console.log(url);
-        console.log(caption);
+        // console.log(url);
+        // console.log(caption);
 
         var temp = document.createElement("div");
         temp.innerHTML = caption;
-
-        // TODO: check if caption is still too long
         document.getElementById("title").innerHTML = stripHTML(caption.substring(0, caption.indexOf('.')));
 
-        // var link = document.createElement('a');
-        // link.href = post_url;
-        // link.innerHTML = caption;
-        // document.querySelectorAll('span.name')[0].appendChild(link).appendChild(link.previousSibling);
-
+        var contributor = getContributor(temp.innerText);
+        document.getElementById("contributor").innerHTML = stripHTML(contributor);
     }
 });
 
@@ -37,4 +32,16 @@ function stripHTML(html) {
     var temp = document.createElement("div");
     temp.innerHTML = html;
     return temp.innerText;
+}
+
+function getContributor(caption) {
+    var index = caption.toLowerCase().indexOf("contributed by");
+    if (index != -1) return caption.substring(index + "contributed by".length + 1).replace(/^[.\s]+|[.\s]+$/g, "");
+    index = caption.toLowerCase().indexOf("submitted by");
+    if (index != -1) return caption.substring(index + "submitted by".length + 1).replace(/^[.\s]+|[.\s]+$/g, "");
+    index = caption.toLowerCase().indexOf("submission by");
+    if (index != -1) return caption.substring(index + "submission by".length + 1).replace(/^[.\s]+|[.\s]+$/g, "");
+    index = caption.toLowerCase().indexOf("contribution by");
+    if (index != -1) return caption.substring(index + + "contribution by".length + 1).replace(/^[.\s]+|[.\s]+$/g, "");
+    return "Cabin Porn"
 }
