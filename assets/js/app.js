d113554519484d7dc57cbc67736d9cb89d51a75a
Andrew Tran <atran@princeton.edu>
2015-05-22 15:46:43 -0700

    ready for app store submission

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 var offset = Math.floor(Math.random() * 1200);
 
 $.ajax({
-    url: "http://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50&offset="+offset,
+    url: "https://api.tumblr.com/v2/blog/cabinporn.com/posts?api_key=1vwk9jz7mQVCYWsZYEhXr1kE2oTN3ClMdNcrobtAPqVAGfk2IH&limit=50&offset="+offset,
     dataType: 'jsonp',
     success: function(results){
     	// console.log(url)
